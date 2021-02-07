fd71ce1bd0c932fcb1b13b577bef63ba1a7efd45
chyn0804 <chyn0804@naver.com>
2021-02-07 15:37:27 +0900

    first

---
+++ src/test/java/com/example1/demo1/Demo1ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.example1.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class Demo1ApplicationTest {
+    @Test
+    public void contextLoads() {
+
+        assertThat("actual", is("actual"));
+    }
+}
